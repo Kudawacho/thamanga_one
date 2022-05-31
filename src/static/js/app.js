612f91b07e476d4911f8e54884c16843d14c1280
Kudawacho <nevastill@yahoo.com>
2022-05-31 14:37:48 +0200

    git going docker

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -106,7 +106,7 @@ function AddItemForm({ onNewItem }) {
                         disabled={!newItem.length}
                         className={submitting ? 'disabled' : ''}
                     >
-                        {submitting ? 'Adding...' : 'Add Summin'}
+                        {submitting ? 'Adding...' : 'Add Summin Dude'}
                     </Button>
                 </InputGroup.Append>
             </InputGroup>
